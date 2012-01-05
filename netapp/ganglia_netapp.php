184fc5d6ec526892ecab5289d37f42536f57d8e5
vuksan <vuksan@mygpslocation.com>
2012-01-05 15:16:12 -0500

    Add Netapp monitoring script

---
+++ netapp/ganglia_netapp.php
@@ -0,0 +1,83 @@
+<?php
+
+# Specify a list of servers you want to monitor
+$servers= array("serv1", "serv2");
+
+# Add any other options you want.
+$gmetric_cmd="/usr/bin/gmetric -d 180 ";
+
+# snmp community
+$community = "public";
+
+# List of OIDs we want monitored
+$absolute_metrics = array(
+  "cpuutil" => ".1.3.6.1.4.1.789.1.2.1.3.0"
+);
+
+$counter_metrics = array(
+  "cifs_ops" => "1.3.6.1.4.1.789.1.2.2.28.0",
+  "nfs_ops" => "1.3.6.1.4.1.789.1.2.2.27.0"
+);
+
+$tmp_stats_file = "/tmp/netappstats";
+
+$old_stats = array();
+
+if ( is_file($tmp_stats_file) ) {
+  $file = fopen ( $tmp_stats_file, "r");
+
+  while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    $exploded = explode(",", $line);
+    if ( sizeof($exploded) > 3 ) 
+      $old_stats[$exploded[1]][$exploded[2]] = array( "time" => $exploded[0],  "value" => $exploded[3]);
+
+    unset($exploded);
+  }
+
+  unlink($tmp_stats_file);
+
+}
+
+
+$output = "";
+
+foreach ( $servers as $index => $server ) {
+
+  # Absolute metrics
+  foreach ( $absolute_metrics as $metric => $oid ) {
+      $value = `snmpwalk -v 2c -c public $server $oid | awk '{ print \$4 }'`;
+      system($gmetric_cmd . " -n netapp_" . $metric . "_" . $server . " -t float -u pct -v " . $value);
+  }
+
+  # Counter metrics
+
+  foreach ( $counter_metrics as $metric => $oid ) {
+      $snmp_value = `snmpwalk -v 2c -c public $server $oid | awk '{ print \$4 }'`;
+      $time = microtime(TRUE);
+
+      $output .= join(",", array($time , $server , $metric, $snmp_value) );
+
+      # Calculate diff
+      if ( isset($old_stats[$server][$metric]) ) {
+	$value = ($snmp_value - $old_stats[$server][$metric]["value"]) / ( $time -$old_stats[$server][$metric]["time"] ) ;
+	# If value is negative counter most likely reset so zero it out
+	if ( $value < 0 )
+	  $value = 0;
+
+      } else {
+	$value = 0;
+      }
+
+      system($gmetric_cmd . " -n netapp_" . $metric . "_" . $server . " -t float -u 'ops/s' -v " . $value);
+
+  }
+
+
+  file_put_contents($tmp_stats_file, $output);
+
+
+
+}
+
+?>
